8d1b756e96edc4a3c2b0091adbf323611458a234
Achraf Belarif <abelarif@e0r1p3.1337.ma>
2021-06-28 15:13:01 +0100

    TODO : fix ft_lstadd_back

--- main.c
+++ main.c
@@ -6,37 +6,18 @@
 /*   By: abelarif <abelarif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 14:11:32 by abelarif          #+#    #+#             */
-/*   Updated: 2021/06/28 14:31:11 by abelarif         ###   ########.fr       */
+/*   Updated: 2021/06/28 15:12:37 by abelarif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_list *ft_lstnew(void *content);
+t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *lst);
 void	print_list(t_list *list);
-void ft_lstadd_front(t_list **lst, t_list *new);
-
-int	main(int argc, char *argv[])
-{
-	t_list	*list;
-	int		i;
-
-	i = 0;
-	list = ft_lstnew(argv[0]);
-	while (++i < argc)
-	{
-		t_list *new;
-
-		new = malloc(sizeof(t_list));
-		if (new == NULL)
-			return (0);
-		new->content = argv[i];
-		new->next = NULL;
-		ft_lstadd_front(&list, new);
-	}
-	print_list(list);
-	return (0);
-}
+void 	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 void	print_list(t_list *list)
 {
@@ -68,5 +49,79 @@ void ft_lstadd_front(t_list **lst, t_list *new)
 
 	tmp_list = *lst;
 	new->next = tmp_list;
-	*lst = new	;
+	*lst = new;
+}
+
+int		ft_lstsize(t_list *lst)
+{
+	int		i;
+
+	i = 0;
+	while (lst != NULL)
+	{
+		i++;
+		lst = lst->next;
+	}
+	return (i);
+}
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	int	lst_size;
+	int	i;
+
+	i = 0;
+	lst_size = ft_lstsize(lst);
+	while (i != lst_size - 1)
+	{
+		i++;
+		lst = lst->next;
+		printf("counter : [%d]\n", i);
+	}
+	return (lst);
+}
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	while (*lst != NULL)
+	{
+		// printf("X\n");
+		if ((*lst)->next == NULL)
+		{
+			printf("X\n");
+			(*lst)->next = new;
+		}
+		else
+		{
+				*lst = (*lst)->next;
+				break ;
+				printf("Y : [%s]\n", (*lst)->content);
+		}
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	t_list	*list;
+	int		i;
+
+	i = 0;
+	list = ft_lstnew(argv[0]);
+	while (++i < argc)
+	{
+		t_list *new;
+
+		new = malloc(sizeof(t_list));
+		if (new == NULL)
+			return (0);
+		new->content = argv[i];
+		new->next = NULL;
+		ft_lstadd_back(&list, new);
+	}
+	print_list(list);
+	printf("len : [%d]\n", ft_lstsize(list));
+	t_list *last = ft_lstlast(list);
+	printf("last : [%s]\n", last->content);
+	print_list(list);
+	return (0);
 }
